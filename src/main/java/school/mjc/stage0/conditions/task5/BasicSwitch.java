47cb6691289e00845401ecfa889a5a47c19218ce
Izzat Tulayev <izzatullohman@gmail.com>
2023-07-26 16:29:18 +0500

    cbdf

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -1,6 +1,6 @@
 package school.mjc.stage0.conditions.task5;
 
-public class BasicSwitch {
+ public class BasicSwitch {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
